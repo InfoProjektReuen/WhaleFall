aca3ded6d83ccb5d7d53ee27b187636c47ff1577
NullPointerExceptionError <schmutzmail69@gmail.com>
2023-02-23 19:36:53 +0100

    .

--- WhaleFall/Assets/Scripts/normalWhaleSpawnerManager.cs
+++ WhaleFall/Assets/Scripts/normalWhaleSpawnerManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerManager : MonoBehaviour
+public class normalWhaleSpawnerManager : MonoBehaviour
 {
     public Camera Camera;
     public GameObject Player;
