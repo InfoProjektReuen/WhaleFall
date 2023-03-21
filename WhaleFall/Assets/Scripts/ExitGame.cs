1fe4a40222ce554b7fb824e00f084328cae6d2af
NullPointerExceptionError <schmutzmail69@gmail.com>
2023-03-21 16:32:21 +0100

    exitGame button & anleitung buttons

---
+++ WhaleFall/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExitGame : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    public void exitGameJo()
+    {
+        Application.Quit();
+    }
+}
